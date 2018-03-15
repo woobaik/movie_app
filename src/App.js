9c67fbacb78d13330fc81c698f87d84ba5b7fe82
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-03-15 12:26:07 -0700

    Learned setTimeout, this.setState({})

--- src/App.js
+++ src/App.js
@@ -26,10 +26,27 @@ const movies = [
 
 ]
 
+
 class App extends Component {
+
+
+  state = {
+    greeting: "Hello"
+  }
+
+
+  componentDidMount() {
+    setTimeout(()=> {
+      this.setState({
+        greeting: "Hello world!"
+      })
+    }, 5000)
+  }
+
   render() {
     return (
       <div className="App">
+        {this.state.greeting}
         {movies.map((movie, index) => {
           return <Movie title= {movie.title} image= {movie.image} key= {index}/>
         })
