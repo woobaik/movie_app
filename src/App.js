bfbf5caef239af2572ae24bcfaecd8e6b9ee74d3
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-03-19 20:02:39 -0700

    Finished practing sumcal script

--- src/App.js
+++ src/App.js
@@ -2,43 +2,49 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
-const movies = [
-
-  {
-    title: "Matrix",
-    image: "https://images-na.ssl-images-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UY1200_CR84,0,630,1200_AL_.jpg"
-  },
-
-  {
-    title: "Okja",
-    image: 'https://images-na.ssl-images-amazon.com/images/M/MV5BMjQxMTcxNDgxN15BMl5BanBnXkFtZTgwOTczNTIzMjI@._V1_UX182_CR0,0,182,268_AL_.jpg'
-  },
-
-  {
-    title: 'Titanic',
-    image: 'https://images-na.ssl-images-amazon.com/images/M/MV5BMDdmZGU3NDQtY2E5My00ZTliLWIzOTUtMTY4ZGI1YjdiNjk3XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_UX182_CR0,0,182,268_AL_.jpg'
-  },
-
-  {
-    title: 'BodyGuard',
-    image: 'https://upload.wikimedia.org/wikipedia/en/thumb/8/8f/The_Bodyguard_1992_Film_Poster.jpg/220px-The_Bodyguard_1992_Film_Poster.jpg'
-  }
-
-]
 
 
 class App extends Component {
 
 
   state = {
-    greeting: "Hello"
+    greeting: "Hello",
+
+    movies: [
+
+      {
+        title: "Matrix",
+        image: "https://images-na.ssl-images-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UY1200_CR84,0,630,1200_AL_.jpg"
+      },
+
+      {
+        title: "Okja",
+        image: 'https://images-na.ssl-images-amazon.com/images/M/MV5BMjQxMTcxNDgxN15BMl5BanBnXkFtZTgwOTczNTIzMjI@._V1_UX182_CR0,0,182,268_AL_.jpg'
+      },
+
+      {
+        title: 'Titanic',
+        image: 'https://images-na.ssl-images-amazon.com/images/M/MV5BMDdmZGU3NDQtY2E5My00ZTliLWIzOTUtMTY4ZGI1YjdiNjk3XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_UX182_CR0,0,182,268_AL_.jpg'
+      },
+
+      {
+        title: 'BodyGuard',
+        image: 'https://upload.wikimedia.org/wikipedia/en/thumb/8/8f/The_Bodyguard_1992_Film_Poster.jpg/220px-The_Bodyguard_1992_Film_Poster.jpg'
+      }
+
+    ]
   }
 
-
   componentDidMount() {
-    setTimeout(()=> {
+    setTimeout(() => {
       this.setState({
-        greeting: "Hello world!"
+        movies: [
+          ...this.state.movies,
+          {
+            title: "Casino Royal",
+            image: 'https://upload.wikimedia.org/wikipedia/en/thumb/8/8f/The_Bodyguard_1992_Film_Poster.jpg/220px-The_Bodyguard_1992_Film_Poster.jpg'
+          }
+        ]
       })
     }, 5000)
   }
@@ -47,7 +53,7 @@ class App extends Component {
     return (
       <div className="App">
         {this.state.greeting}
-        {movies.map((movie, index) => {
+        {this.state.movies.map((movie, index) => {
           return <Movie title= {movie.title} image= {movie.image} key= {index}/>
         })
 
